a4b0296eae219114708ae5bbd5df574ea4a52ac5
Ayxan Mustafayev <118119951+ayxansumqayit@users.noreply.github.com>
2024-09-15 00:18:23 +0400

    gallery section added

---
+++ about/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".about__carousel").owlCarousel({
+        items: 5, 
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true
+    });
+});
